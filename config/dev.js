8698e134cc48589d1a04197881293cf4eee98aa3
rajatchauhan407 <38553891+rajatchauhan407@users.noreply.github.com>
2022-07-31 21:33:18 +0530

    Delete config directory

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '357888347936-t044jniqpmrc0ubjkgrn4h50vggi38uv.apps.googleusercontent.com',
-  googleClientSecret: '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',
-  mongoURI: 'mongodb+srv://rajat_veggi1304:wHJZXoermTUGyShc@veggies.znzgp.mongodb.net/?retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl:'redis://127.0.0.1:6379'
-};
